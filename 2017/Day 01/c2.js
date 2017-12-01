85200ded9eabd8072a2d29b79b7d513a4335ddb7
Mark Johnston <mark.johnston@idesigni.co.uk>
2017-12-01 14:01:23 +0000

    Added solution to part 2 and cleaned up part 1

--- 2017/Day 01/c2.js
+++ 2017/Day 01/c2.js
@@ -3,7 +3,7 @@ const input = require('fs').readFileSync('input.txt').toString().trim();
 let total = 0;
 
 for (var i = 0; i < input.length; i++) {
-  const next =  input[(i + 1) % input.length];
+  const next =  input[(i + input.length / 2) % input.length];
   if (input[i] === next) { total += parseInt(input[i], 10); }
 }
 
