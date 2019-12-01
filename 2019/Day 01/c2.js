7d82c5ef77f023dc4d075afd94ff7ed600d41368
Mark Johnston <mjohnston89@gmail.com>
2019-12-01 22:45:53 +0000

    Cleaned up part 1 and added solution for part 2

--- 2019/Day 01/c2.js
+++ 2019/Day 01/c2.js
@@ -1,12 +1,17 @@
 const calc = x => Math.floor(x / 3) - 2;
 const sum = (acc, x) => acc + x;
 
+const fuelTotal = (x) => {
+  const fuel = calc(x);
+  return (fuel > 0) ? fuel + fuelTotal(fuel) : 0;
+}
+
 const total = require('fs')
   .readFileSync('input.txt')
   .toString()
   .trim()
   .split("\n")
-  .map(calcFuel)
+  .map(fuelTotal)
   .reduce(sum);
 
 console.log(total);
